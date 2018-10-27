19dd67f7af86b6ea7f2d029e7394a40caccd4df7
Noop5574 <44319316+Noop5574@users.noreply.github.com>
2018-10-27 17:04:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -229,8 +229,8 @@ function play(guild, song) {
     serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }
  
-const adminprefix = "$vip";
-const devs = ['274923685985386496'];
+const adminprefix = "w";
+const devs = ['256447238698434562'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
